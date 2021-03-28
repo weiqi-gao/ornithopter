960377d8271846a40e0bb32242ff290707fb9319
Kirk Boyd <kirkland.boyd@trincoll.edu>
2021-03-28 19:28:04 -0400

    write code to test, need to try this with other board than bluno mega i think...

--- hallEffectTest/hallEffectTest.ino
+++ hallEffectTest/hallEffectTest.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#define hallpin1 6 //analog pin 6
+#define hallpin2 7 //analog pin 7
 
+void setup() {
+  pinMode(hallpin1, INPUT);
+  pinMode(hallpin2, INPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Serial.print("Hall 1 (board): ");
+  //Serial.print(analogRead(hallpin1));
+  Serial.print("Hall: ");
+  Serial.println(analogRead(hallpin2));
 
 }

ea3e99832f9634960033d32fc5de214777dacb10
KirkBoyd <40477730+KirkBoyd@users.noreply.github.com>
2021-04-24 20:15:46 -0400

    store changes from pc

--- wireless/flappyBirdOnboardWithAccel/flappyBirdOnboardWithAccel.ino
+++ wireless/flappyBirdOnboardWithAccel/flappyBirdOnboardWithAccel.ino
@@ -10,6 +10,13 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <PWMServo.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/* Constants */
+#define BNO055_sampleRate //delay in ms of samples
 
 /* Pins/Ports */
 #define escPin 2 //digital pin 2 for pwm signal
@@ -25,11 +32,17 @@ PWMServo ESC;
 const int pwmMin = 1000;
 const int pwmMax = 2000;
 
+/* Accelerometer Setup */
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
+
 /* Variables */
 int pot1 = 0;
+float accelData[2];
+long allData[5];
+long yAccel;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   radio.begin();
   radio.openWritingPipe(addresses[1]);
   radio.openReadingPipe(1, addresses[0]);
@@ -39,21 +52,47 @@ void setup() {
   pinMode(hallPin2,INPUT); 
   //pinMode(escPin,OUTPUT); //we will write PWM signals to control the motor speed
   ESC.attach(escPin,pwmMin,pwmMax);
+  if(!bno.begin()){
+    Serial.println("No BNO055 detected.");
+    while(1);
+  }
+  bno.setExtCrystalUse(true);
 }
 
 void loop() {
+  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   radio.startListening();
   delay(3);
   if(radio.available()){ //check if there is a signal coming in
-    radio.read(&pot1,sizeof(pot1)); //recieve potentiometer value
-    Serial.println(pot1);
-    ESC.write(pot1); //write potentiometer val to esc
+    //while(radio.available()){
+      radio.read(&pot1,sizeof(pot1)); //recieve potentiometer value
+      Serial.print("Pot: ");
+      Serial.println(pot1);
+      //Serial.print("\t");
+      ESC.write(pot1); //write potentiometer val to esc
+    //}
+    radio.stopListening();
   }
+  else{ 
+    //Serial.print("\t\t");
+    //radio.stopListening();
+  }
+  
   radio.stopListening();
   int data1[] = {analogRead(hallPin1),analogRead(hallPin2)};
+  accelData[0] = accel.x();
+  accelData[1] = accel.y();
   Serial.print(data1[0]);
   Serial.print(" ");
-  Serial.println(data1[1]);
+  Serial.print(data1[1]);
+  Serial.print(" ");
   radio.write(&data1[0], sizeof(data1[0]));
+  //delay(5);
   radio.write(&data1[1], sizeof(data1[1]));
+  delay(20);
+  radio.write(&accelData[0],sizeof(accelData[0]));
+  //delay(5);
+  radio.write(&accelData[1],sizeof(accelData[1]));
+  //delay(5);
+  Serial.println();
 }
--- wireless/flappyBirdRemote/flappyBirdRemote.ino
+++ wireless/flappyBirdRemote/flappyBirdRemote.ino
@@ -9,6 +9,10 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
 
 /* Pins/Ports */
@@ -18,8 +22,15 @@
 RF24 radio(7, 8); // CE, CSN
 const byte addresses[][6] = {"10501","10502"};
 
+/* Dynamic Variables */
+int pot1raw = 0;
+int pot1map = 0;
+int data1[2];
+int accelData[3];
+
+/* Setup Loop */
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(potPin,INPUT);
   radio.begin();
   radio.openWritingPipe(addresses[0]);
@@ -29,20 +40,37 @@ void setup() {
 }
 
 void loop() {
-  radio.stopListening();
-  int pot1 = analogRead(A0);
-  pot1 = map(pot1, 0, 1023, 0, 180);
-  Serial.println(pot1);
-  radio.write(&pot1, sizeof(pot1));
-//  if (radio.available()) {
-//    int data1[2];
-//    radio.read(&data1[0], sizeof(data1[0]));
-//    radio.read(&data1[1], sizeof(data1[1]));
-//    Serial.print(data1[0]);
-//    Serial.print(" ");
-//    Serial.println(data1[1]);
-//    char text[32] = "";
-//    radio.read(&text, sizeof(text));
-//    Serial.println(text);
-//  }
+  
+  if (map(analogRead(A0),0,1023,0,180) != pot1raw){
+    radio.stopListening();    
+    pot1raw = analogRead(A0);
+    pot1raw = map(pot1raw, 0, 1023, 0, 180);
+    Serial.print("Pot: ");
+    Serial.print(pot1raw);
+    radio.write(&pot1raw, sizeof(pot1raw));
+    Serial.print("\t\t");
+  }
+  else {Serial.print("\t\t");}
+  //delay(5);
+  radio.startListening();
+  if (radio.available()) {
+    radio.read(&data1[0], sizeof(data1[0]));
+    radio.read(&data1[1], sizeof(data1[1]));
+    radio.read(&accelData[0], sizeof(accelData[0]));
+    radio.read(&accelData[1], sizeof(accelData[1]));
+    radio.read(&accelData[2], sizeof(accelData[2]));
+    Serial.print("Hall Sensors: ");
+    Serial.print(data1[0]);
+    Serial.print(" ");
+    Serial.print(data1[1]);  
+    Serial.print("\t");
+    Serial.print("Accelerometer: ");
+    Serial.print(accelData[0]);
+    Serial.print(" ");
+    Serial.print(accelData[1]);
+    Serial.print(" "); 
+    Serial.print(accelData[2]);
+    Serial.print(" ");   
+  }
+  Serial.println();
 }
